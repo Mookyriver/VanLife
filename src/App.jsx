32ef8ea7e528bea0f5229c393606cb2cecc7be86
Mookyriver <mookyriver@gmail.com>
2025-04-21 01:19:53 +0200

    adding files to git

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route , Link} from 'react-router-dom'
+
+
+import Home from './Pages/Home'
+import About from './Pages/About'
+
+function App() {
+  
+
+  return (
+    <>
+     <BrowserRouter>
+
+      <header>
+
+        <Link className='site-logo' to='/'>#VANLIFE</Link>
+        <nav>
+
+        <Link to='/about'>About</Link>
+
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+
+      </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
